c6d5e98f2792bd643b7315df868079ca9f42fbf6
weaponsforge <weaponsforge@users.noreply.github.com>
2024-08-17 18:04:04 +0000

    deploy: c948e50c33ed5dc534a6cea18b53dfc520a62158

---
+++ _next/static/HYMXe953z8lwDtwz1RBjY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/00ab5c8b2cf3fb8e.css","static/chunks/pages/index-fd38a073d006a7a1.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
